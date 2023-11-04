c7cba40d5b304d8b7f3f63e70bea998a108175cc
Aleksey Zolotarev <alexiy_54@mail.ru>
2023-11-04 09:02:53 +0300

    3

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -4,6 +4,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Deth8 {
-//    @Override
-//    public String toString () {return "";}
+    @Override
+    public String toString () {return "";}
 }
